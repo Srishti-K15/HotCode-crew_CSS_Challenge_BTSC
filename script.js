39a360b4cda7d15393c68235fec46ce7576d0e90
Srishti-K15 <srishtikjaiswal@gmail.com>
2024-07-14 18:25:54 +0530

    buttons

--- script.js
+++ script.js
@@ -2,49 +2,53 @@ const ball = document.querySelector('.ball');
 const startBtn = document.querySelector('#start-btn');
 const resumeBtn = document.querySelector('#resume-btn');
 const stopBtn = document.querySelector('#stop-btn');
-const scoreboard = document.querySelector('#score');
-let animationRunning = false;
+let animationRunning = true;
 let animationPaused = false;
-let score = 0;
 
-function moveBall() {
-    if (animationRunning) {
-        ball.style.animation = 'move-ball 5s linear infinite';
-      }
-      
-    
-  }
-
-startBtn.addEventListener('click', () => {
-    
-    ball.style.animation = 'move-ball 5s linear infinite';
+function startBallAnimation() {
+    ball.style.animation = `none`; 
+    ball.offsetHeight; 
+    ball.style.animation = `move-ball 8s linear infinite`;
+    ball.style.animationPlayState = 'running';
     animationRunning = true;
     animationPaused = false;
-    
-    score = 0;
-    scoreboard.textContent = ` ${score}`;
+    resumeBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function pauseBallAnimation() {
+    ball.style.animationPlayState = 'paused';
+    animationRunning = false;
+    animationPaused = true;
+    startBtn.disabled = false;
+    resumeBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', () => {
+    ball.style.left = '10px';  
+    ball.style.top = '25px';   
+    startBallAnimation();
+    startBtn.disabled = true; 
 });
 
 resumeBtn.addEventListener('click', () => {
-    
-    ball.style.animation = 'move-ball 5s linear infinite';
-    animationRunning = true;
-    animationPaused = false;
-    
-    score = 0;
-    scoreboard.textContent = ` ${score}`;
-    // animationRunning = true;
-    // animationPaused = false;
+    if (animationPaused) {
+        ball.style.animationPlayState = 'running';
+        animationRunning = true;
+        animationPaused = false;
+        resumeBtn.disabled = true;
+        stopBtn.disabled = false;
+    }
 });
 
 stopBtn.addEventListener('click', () => {
-    ball.style.animation = 'none';
-   
-
+    if (animationRunning) {
+        pauseBallAnimation();
+        resumeBtn.disabled = false;
+        stopBtn.disabled = true;
+    }
 });
-
-// Update score when ball reaches the end of the maze
-ball.addEventListener('animationiteration', () => {
-    score++;
-    scoreboard.textContent = `${score}`;
+window.addEventListener('load', () => {
+    startBallAnimation();
+    startBtn.disabled = true; 
 });
